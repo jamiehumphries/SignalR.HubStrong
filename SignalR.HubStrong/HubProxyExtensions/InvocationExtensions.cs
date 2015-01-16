e517b80d9f1649c1b89941e111c52baed0984ef4
Jamie Humphries <jamie.humphries@softwire.com>
2015-01-16 11:32:47 +0000

    Add methods to create IHubProxy<T> objects.

---
+++ SignalR.HubStrong/HubProxyExtensions/InvocationExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace SignalR.HubStrong.HubProxyExtensions
+{
+    using Microsoft.AspNet.SignalR.Client;
+
+    public static class InvocationExtensions
+    {
+        public static IHubProxy<T> CreateHubProxy<T>(this HubConnection hubConnection, string hubName) where T : class
+        {
+            return hubConnection.CreateHubProxy(hubName).AsProxyFor<T>();
+        }
+
+        public static IHubProxy<T> AsProxyFor<T>(this IHubProxy hubProxy) where T : class
+        {
+            return new HubProxy<T>(hubProxy);
+        }
+    }
+}
